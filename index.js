26605cdb0f00e3837e0cc9d61f44749fc821d4cc
LiamHz <liam.hinzman@gmail.com>
2018-05-02 12:13:19 -0400

    Heroku Support
    Heroku support

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+// Heroku support
+
+const express = require('express')
+const path = require('path')
 const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+// END Heroku support
 
 var app = require("express")();
 var http = require("http").Server(app);
