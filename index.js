f18ed15f3f57e6223001d149abed11b5ebd0d1d5
LiamHz <liam.hinzman@gmail.com>
2018-05-02 07:51:27 -0400

    Socket.io Emit
    Message box submit now emits a NodeJS call. Logs message text.

--- index.js
+++ index.js
@@ -7,12 +7,15 @@ app.get("/", function(req, res){
 });
 
 io.on("connection", function(socket){
-  console.log("a user connected");
-  socket.on("disconnect", function(){
-      console.log("user disconnected")
+  socket.on("chat message", function(msg){
+    console.log("message: " + msg);
   });
+  // console.log("a user connected");
+  // socket.on("disconnect", function(){
+  //     console.log("user disconnected")
+  // });
 });
 
 http.listen(3000, function(){
   console.log("listening on *:3000");
-})
+});
