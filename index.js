efe132412bbddfea5dba546631410cb0443d6592
LiamHz <liam.hinzman@gmail.com>
2018-05-02 08:08:45 -0400

    Chat message v1.0
    User can now send chat meessages that display to all other active users.

--- index.js
+++ index.js
@@ -7,13 +7,16 @@ app.get("/", function(req, res){
 });
 
 io.on("connection", function(socket){
+  console.log("a user connected");
+
   socket.on("chat message", function(msg){
-    console.log("message: " + msg);
+    // When a user sends a message, broadcast the message to all other users
+    io.emit("chat message", msg);
   });
-  // console.log("a user connected");
-  // socket.on("disconnect", function(){
-  //     console.log("user disconnected")
-  // });
+
+  socket.on("disconnect", function(){
+      console.log("user disconnected")
+  });
 });
 
 http.listen(3000, function(){
