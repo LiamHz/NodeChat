0d1cf8a14c4ff86610a74f986ba1933b231ad0b6
Liam Hinzman <liam.hinzman@gmail.com>
2018-08-26 11:37:52 -0400

    Users are now notified when a new user joins the chatroom

--- index.js
+++ index.js
@@ -11,21 +11,23 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', path.join(__dirname, 'views'));
 
 app.get("/", function(req, res){
-  res.sendFile(__dirname + "/views/pages/index.html");
+    res.sendFile(__dirname + "/views/pages/index.html");
 });
 
 io.on("connection", function(socket){
-  console.log("a user connected");
 
-  socket.on("chat message", function(msg){
-      console.log('message: ' + msg)
-    // When a user sends a message, broadcast the message to all other users
-    io.emit("chat message", msg);
-  });
+    console.log("a user connected");
+    io.emit("user connected")
 
-  socket.on("disconnect", function(){
-      console.log("user disconnected")
-  });
+    socket.on("chat message", function(msg){
+        console.log('message: ' + msg)
+        // When a user sends a message, broadcast the message to all other users
+        io.emit("chat message", msg);
+    });
+
+    socket.on("disconnect", function(){
+        console.log("user disconnected")
+    });
 });
 
 http.listen(PORT, () => console.log(`Listening on ${ PORT }`));
